2bb13009d3e50d49bba99b5ea11db440fb82bfee
pnb90 <phuocbui90@gmail.com>
2019-01-10 11:14:31 -0600

    inheritance_modules

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,12 +1,12 @@
-class Bike
-  attr_reader :speed
+class Vehicle
+  attr_reader :speed, :direction
 
-  def initialize
+  def initialize #attributes - characteristics of a object; can be rewritten
     @speed = 0
     @direction = 'north'
   end
 
-  def brake
+  def brake #behavior - something the object does, usually created through methods
     @speed = 0
   end
 
@@ -18,25 +18,59 @@ class Bike
     @direction = new_direction
   end
 
+end
+
+class Bike < Vehicle
+  attr_reader :gear, :weight, :type
+
+  def initialize(input_options)
+    super()
+    @gear = input_options[:gear]
+    @weight = input_options[:weight]
+    @type = input_options[:type]
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
-end
 
-class Car < Bike
+end 
+
+class Car < Vehicle
+  attr_reader :fuel, :make, :model #to easily make an attr reader, R + tab
+  attr_writer :fuel, :make, :model #to easily make an attr writer, W + tab
+  
+  def initialize(input_options)
+    super()
+    @make = input_options[:make]
+    @model = input_options[:model]
+    @fuel = input_options[:fuel]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-car = Car.new
-bike = Bike.new
+car = Car.new(
+              make: "Toyota", 
+              model: "Prius", 
+              fuel: 20,
+              )
 
-bike.accelerate
-p bike.speed
+bike = Bike.new(make: "fixie", 
+                weight: "15 pound",
+                type: "roadbike",
+                gear: "1 gear"
+                )
+
+p car
+p bike
 
 car.accelerate
-p car.accelerate
+bike.accelerate
 
-bike.ring_bell
-car.honk_horn
+p car
+p bike
+
+
